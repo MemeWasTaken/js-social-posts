0a317f12b3ff7891416d2f0bebb3b047d24c91bc
MemeWasTaken <memelazzari02@gmail.com>
2021-12-10 14:58:51 +0100

    problem in words

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// 1) Creazione array di oggetti
+    // - Ogni oggetto deve contenere:
+        // a) nome autore;
+        // b) foto profilo;
+        // c) data (formato americano);
+        // d) testo del post;
+        // e) Immagine;
+        // f) numero likes
+// 2) Stampare post nel DOM
+// 3) Creare Buttone dei likes cliccabile
