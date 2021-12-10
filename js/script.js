35620319ba7787563f0720a2cacc0c43cae943c2
MemeWasTaken <memelazzari02@gmail.com>
2021-12-10 17:05:06 +0100

    btn to be continued

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 const post = [
     {
         authorName: 'Phil Mangione',
-        profilePic: 'https://unsplash.it/300/300?image=15',
+        profilePic: 'https://unsplash.it/300/300?image=',
         data: '08/10/2021',
         caption: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://unsplash.it/600/300?image=171',
@@ -27,7 +27,7 @@ const post = [
     },
     {
         authorName: 'Vito Alloggio',
-        profilePic: 'https://unsplash.it/300/300?image=15',
+        profilePic: 'https://unsplash.it/300/300?image',
         data: '08/10/2021',
         caption: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://unsplash.it/600/300?image=171',
@@ -35,7 +35,7 @@ const post = [
     },
     {
         authorName: 'Guido La Moto',
-        profilePic: 'https://unsplash.it/300/300?image=15',
+        profilePic: 'https://unsplash.it/300/300?image=187',
         data: '08/10/2021',
         caption: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://unsplash.it/600/300?image=171',
@@ -43,7 +43,7 @@ const post = [
     },
     {
         authorName: 'Massimo Delapena',
-        profilePic: 'https://unsplash.it/300/300?image=15',
+        profilePic: 'https://unsplash.it/300/300?image=203',
         data: '03/25/2021',
         caption: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://unsplash.it/600/300?image=171',
@@ -62,41 +62,52 @@ function calcDate(postDate) {
 
 // 2) Stampare post nel DOM
 // SECTION post filler //
-  const containerPost = document.getElementById('container');
-  for (let i = 0; i < post.length; i++) {
-    const obj = post[i];
-    const templatePost = `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${obj.profilePic}" alt="${obj.authorName}">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${obj.authorName}</div>
-                        <div class="post-meta__time">${calcDate(obj.data)} giorni fa</div>
-                    </div>                    
+const containerPost = document.getElementById('container');
+for (let i = 0; i < post.length; i++) {
+const obj = post[i];
+const templatePost = `
+<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${obj.profilePic}" alt="${obj.authorName}">                    
                 </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${obj.authorName}</div>
+                    <div class="post-meta__time">${calcDate(obj.data)} giorni fa</div>
+                </div>                    
             </div>
-            <div class="post__text">${obj.caption}</div>
-            <div class="post__image">
-                <img src="${obj.postImage}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${obj.numberLikes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>`;
+        </div>
+        <div class="post__text">${obj.caption}</div>
+        <div class="post__image">
+            <img src="${obj.postImage}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${obj.numberLikes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`;
     containerPost.innerHTML += templatePost;
-  }
-// 3) Creare Bottone dei likes cliccabile
+
+    // 3) Creare Bottone dei likes cliccabile
+    let btnLike = document.querySelector('.like-button');
+    let numberLike = document.getElementById('like-counter-1');
+    btnLike.addEventListener('click', function() {
+        btnLike.classList.add('like-button--liked');
+        if (btnLike.classList.contains('like-button--liked')) {
+            numberLike.innerText = obj.numberLikes;
+            console.log(obj.numberLikes);
+        }
+    });
+}
+
 
